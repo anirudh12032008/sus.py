1e8f4577285019ad35eae0528c21f6b513a41770
Anirudh Sahu <renudilip19@gmail.com>
2026-07-17 14:25:57 +0530

    added jitters as well

--- face.py
+++ face.py
@@ -10,15 +10,15 @@ MODEL_P = "models/m.task"
 
 REYE = (33, 160, 158, 133, 153, 144)
 LEYE = (362, 385, 387, 263, 373, 380)
-# MOUTHP = (61, 291, 78, 308)
-# BROWP = (70, 63, 105, 66, 107, 300, 293, 3334, 296, 336)
-# JITTERP = MOUTHP + BROWP
-# IODP = (33, 263)
+MOUTHP = (61, 291, 78, 308)
+BROWP = (70, 63, 105, 66, 107, 300, 293, 334, 296, 336)
+JITTERP = MOUTHP + BROWP
+IODP = (33, 263)
 BLINK_EAR = 0.21
-MIN_BLINK = 1
+MIN_BLINK = 2
 MAX_BLINK = 12
-# BLINK_WIN = 10.0
-# JITTER_WIN = 2.0
+BLINK_WIN = 10.0
+JITTER_WIN = 2.0
 
 
 def eye_ratio(pts):
@@ -29,73 +29,6 @@ def eye_ratio(pts):
         return 0.0
     return vert / (2.0*hor)
 
-
-# class FaceSignals:
-#     def __init__(self, model=MODEL_P):
-#         base = mp_python.BaseOptions(model_asset_path=model)
-#         self._landmarker = vision.FaceLandmarker.create_from_options(
-#             vision.FaceLandmarkerOptions(
-#                 base_options = base,
-#                 running_mode = vision.RunningMode.VIDEO,
-#                 num_faces = 1,
-#             )
-#         )
-#         self._blinks = deque()
-#         self._jitters = deque()
-#         self._closedf = 0
-#         self._prevpts = None
-#         self._start = time.monotonic()
-
-#     def _topix(self, landmarks, w, h):
-#         return np.array([[lm.x*w, lm.y *h] for lm in landmarks], dtype=np.float64)
-    
-#     def _uptblinks(self, ear, now):
-#         if ear < BLINK_EAR:
-#             self._closedf += 1
-#         else:
-#             if MIN_BLINK <= self._closedf <= MAX_BLINK:
-#                 self._blinks.append(now)
-#             self._closedf=0
-#         while self._blinks and now - self._blinks[0] > BLINK_WIN:
-#             self._blinks.popleft()
-#     def _blinkrate(self, now):
-#         elp = min(now - self._start, BLINK_WIN)
-#         if elp < 1.0:
-#             return 0.0
-#         return len(self._blinks) * (60.0/elp)
-    
-#     def _uptjitter(self, pts, now):
-#         reg = pts[list(JITTERP)]
-#         iod = np.linalg.norm(pts[IODP[0]] - pts[IODP[1]])
-
-#         if self._prevpts is not None and iod > 1e-6:
-#             d = reg - self._prevpts
-#             d -= d.mean(axis=0)
-#             res = np.linalg.norm(d, axis=1).mean()/iod
-#             self._jitters.append((now, res))
-#         self._prevpts = reg
-#         while self._jitters and now - self._jitters[0][0] > JITTER_WIN:
-#             self._jitters.popleft()
-    
-#     def _jscore(self):
-#         if not self._jitters:
-#             return 0.0
-#         return float(np.mean([v for _, v in self._jitters]))
-    
-#     def process(self, f, ts):
-#         h, w = f.shape[:2]
-#         rgb = f[:,:,::-1]
-#         img = Image(image_format=ImageFormat.SRGB, data=np.ascontiguousarray(rgb))
-#         res = self._landmarker.detect_for_video(img, ts)
-#         now = time.monotonic()
-#         if not res.face_landmarks:
-#             self._prevpts = None
-#             return {
-#                 'blink_rate': self._blinkrate(now),
-#                 "jitter_score": self._jscore(),
-#                 "ear": None,
-#                 "face_found": False,
-#             }
 
 def topix(landmarks, w, h):
     return np.array([[lm.x*w, lm.y *h] for lm in landmarks])
@@ -123,7 +56,12 @@ blinks =0
 closedf = 0
 fps = 0.0
 lastt = time.time()
+blinkt = deque()
 
+
+prevpts = None
+jitters = deque()
+jscore = 0.0
 
 while True:
     ok, f = c.read()
@@ -151,8 +89,54 @@ while True:
         else:
             if MIN_BLINK <= closedf <= MAX_BLINK:
                 blinks += 1
+                blinkt.append(now)
                 print("%d for %d frames" % (blinks, closedf)) 
             closedf = 0
+
+
+        while blinkt and now - blinkt[0] > BLINK_WIN:
+            blinkt.popleft()
+        secs = min(now - t0, BLINK_WIN)
+        if secs < 1.0:
+            rate = 0.0
+        else:
+            rate = len(blinkt) * (60.0/secs)
+
+        
+
+
+
+
+
+
+
+
+
+
+
+        # holy interesting part
+        reg = pix[list(JITTERP)]
+        iod = np.linalg.norm(pix[IODP[0]] - pix[IODP[1]])
+
+        if prevpts is not None and iod > 1e-6:
+            d = reg - prevpts
+            d = d - d.mean(axis=0)
+            jit = np.linalg.norm(d, axis=1).mean()/iod
+            jitters.append((now, jit))
+
+        prevpts=reg
+        while jitters and now - jitters[0][0]> JITTER_WIN:
+            jitters.popleft()
+        
+        if jitters:
+            jscore = float(np.mean([v for _, v in jitters]))
+        else:
+            jscore=0.0
+        
+
+
+        
+        # drawing time
         for p in pts:
             x = int(p.x *w)
             y = int(p.y * h)
@@ -161,10 +145,12 @@ while True:
         cv2.putText(f, "EAR %.3f" % ear, (20,40), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
         # cv2.putText(f, "EAR %.3f" % ear, (20,40), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
         cv2.putText(f, "blinks %d" % blinks, (20,80), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+        cv2.putText(f, "blinks/min %.1f" % rate, (20,160), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+        cv2.putText(f, "jitter %.4f" % jscore, (20,200), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
     else:
         cv2.putText(f, "noface", (20,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
+        prevpts = None
     cv2.putText(f, "fps %.0f" % fps, (20,120), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
-
     cv2.imshow("sus", f)
     k = cv2.waitKey(1)
     
